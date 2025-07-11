be73b9f85dc10df4dfe61df24d33ba73e4c9dad4
Felipe Boaventura <correa.felip3@gmail.com>
2025-07-11 00:48:33 -0300

    Desafio concluído

--- conta-banco/src/main/java/io/github/felipeboaventura/ContaTerminal.java
+++ conta-banco/src/main/java/io/github/felipeboaventura/ContaTerminal.java
@@ -1,8 +1,26 @@
 package io.github.felipeboaventura;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Olá, gostaria de algumas informações.");
+        System.out.print("Nome: ");
+        String name = sc.nextLine();
+
+        System.out.print("\nNúmero da conta: ");
+        int number = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("\nAgência com o dígito: ");
+        String agency = sc.nextLine();
+
+        System.out.print("\nSaldo: ");
+        int balance = sc.nextInt();
+
+        System.out.println("Olá " + name + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + number + " e seu saldo R$" + balance + " já está disponível para saque.");
 
     }
 }
